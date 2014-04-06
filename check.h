6cead3f7e56417577cce7b81eaf9144e8d1be8b2
Pentium44 <cdorm245@gmail.com>
2014-04-06 17:56:33 +0800

    New file tree

--- check.h
+++
@@ -1,6 +0,0 @@
-// CTHTTPD - Simple Web Server - GPLv2
-// Chris Dorman, 2012-2014 (cddo@riseup.net)
-// Help from Nickolai Vurgaft (slipperygate@gmail.com)
-
-int file_exists(char *fname);
-int is_dir(char* p);
